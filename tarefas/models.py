19604d3eeaf50d540033055926cad6c2cbd562f4
Rychardsson <rychardssong2@gmail.com>
2024-09-11 15:49:58 -0300

    Update models.py

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    date = models.DateField()
+    time = models.TimeField(blank=True, null=True)
+    google_calendar_id = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
